34140f695b5adfd796a7ccd43a6a66e6a75efb57
Igor PC <igor_roc@hotmail.com.br>
2023-12-08 23:51:01 -0300

    feat: WIP page `cadastrar`

---
+++ src/app/cadastrar/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link"
+import React from "react"
+
+export default function Cadastrar() {
+	return (
+		<div className="flex flex-col min-h-screen items-center justify-center">
+			<p>Em desenvolvimento...</p>
+			<Link href="/">Voltar para o início</Link>
+		</div>
+	)
+}
